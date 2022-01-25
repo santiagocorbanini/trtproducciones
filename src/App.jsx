e119eb8d70a581de8296066da23eae2be37ec092
Franco Vanney <francoavanney@gmail.com>
2022-01-25 15:32:09 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Masterhead from "./components/Masterhead/Masterhead";
+import Nav from "./components/Nav/Nav";
+import Portfolio from "./components/Portfolio/Portfolio";
+
+
+const App = () => {
+  return (
+    <>
+    <Nav/>
+    <Masterhead/>
+    <Portfolio />
+    </>
+  );
+};
+
+export default App;
