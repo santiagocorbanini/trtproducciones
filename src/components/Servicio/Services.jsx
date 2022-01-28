2f1fb0570e9a46234cfc2cdc7e86bd1cd6b03ea2
Franco Vanney <francoavanney@gmail.com>
2022-01-28 14:50:35 -0300

    servicios

--- src/components/Servicio/Services.jsx
+++ src/components/Servicio/Services.jsx
@@ -1,75 +1,41 @@
+import gira from '../../assets/img/gira2022.jpg'
+import distribucion from '../../assets/img/distribucion.jpg'
+import contenido from '../../assets/img/contenidoAudiovisual.jpg'
+import estudios from '../../assets/img/estudios.jpg'
+import produccion from '../../assets/img/produccion.jpg'
+
 
 const Services = () => {
     return (
-        <section className="page-section portfolio bg-primary" id="services">
-                <div className="container">
-                    {/*<!-- Portfolio Section Heading-->*/}
-                    <h2 className="page-section-heading text-center text-uppercase text-black mb-0">Servicios</h2>
+        <section className="page-section bg-primary text-white mb-0 text-center" id="about">
+                    <h2 className="page-section-heading text-center text-uppercase text-black">Nosotros</h2>
                     {/*<!-- Icon Divider -->*/}
-                    <div className="divider-custom">
+                    <div className="divider-custom divider-dark">
                         <div className="divider-custom-line"></div>
-                        <div className="divider-custom-icon"><i className="fas fa-music"></i></div>
+                        <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
                         <div className="divider-custom-line"></div>
                     </div>
-                    {/*<!-- Portfolio Grid Items-->*/}
-                    <div className="row justify-content-center">
-                        {/*<!-- Portfolio Item 1 -->*/}
-                        <div className="col-md-6 col-lg-4 mb-5">
-                            <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
-                                <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                    <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                    <div className="container text-black">
+                        <div className="row">
+                                {/*<!-- Servicios Descripcion -->*/}
+                                <div className="col-lg-8 text-left d-flex flex-column pt-5" id="nosotros">
+                                <span className=""><i class="fas fa-guitar fa-lg pr-2"></i> Brindamos todas las comodidades necesarias para que los músicos puedan desarrollar su arte de la mejor manera.</span>
+                                <span className=""><i class="fas fa-headphones-alt fa-lg pr-2"></i> Nos encargamos de gestionar la distribución digital del material de nuestros artistas en todas las plataformas digitales.</span>
+                                <span className=""><i class="fas fa-clipboard fa-lg pr-2"></i> Junto a ellos, planificamos las salidas de nuevos lanzamentos, singles, videos o álbumes y Hacemos un seguimiento y estudio de las estadísticas para saber en que puntos mejorar, y así lograr los mayores resultados.</span>
+
                                 </div>
-                                <img className="img-fluid" src="assets/img/portfolio/cabin.png" alt="..." />
-                            </div>
-                        </div>
-                        {/*<!-- Portfolio Item 2 -->*/}
-                        <div className="col-md-6 col-lg-4 mb-5">
-                            <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal2">
-                                <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                    <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                                {/*<!-- Imagenes Servicios-->*/}
+                                <div className="col-lg-4 pt-5 mb-lg-0">
+                                    <div className='cuadrado-img'>
+                                    <img className='w-50 border-tl' src={distribucion} alt="" srcset="" />
+                                    <img className='w-50 border-tr' src={contenido} alt="" srcset="" />
+                                    <img className='w-50 border-bl' src={estudios} alt="" srcset="" />
+                                    <img className='w-50 border-br' src={produccion} alt="" srcset="" />
+                                    </div>
                                 </div>
-                                <img className="img-fluid" src="assets/img/portfolio/cake.png" alt="..." />
-                            </div>
-                        </div>
-                        {/*<!-- Portfolio Item 3 -->*/}
-                        <div className="col-md-6 col-lg-4 mb-5">
-                            <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal3">
-                                <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                    <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                                </div>
-                                <img className="img-fluid" src="assets/img/portfolio/circus.png" alt="..." />
-                            </div>
-                        </div>
-                        {/*<!-- Portfolio Item 4 -->*/}
-                        <div className="col-md-6 col-lg-4 mb-5 mb-lg-0">
-                            <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal4">
-                                <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                    <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                                </div>
-                                <img className="img-fluid" src="assets/img/portfolio/game.png" alt="..." />
-                            </div>
-                        </div>
-                        {/*<!-- Portfolio Item 5 -->*/}
-                        <div className="col-md-6 col-lg-4 mb-5 mb-md-0">
-                            <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal5">
-                                <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                    <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                                </div>
-                                <img className="img-fluid" src="assets/img/portfolio/safe.png" alt="..." />
-                            </div>
-                        </div>
-                        {/*<!-- Portfolio Item 6 -->*/}
-                        <div className="col-md-6 col-lg-4">
-                            <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal6">
-                                <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                    <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                                </div>
-                                <img className="img-fluid" src="assets/img/portfolio/submarine.png" alt="..." />
-                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
+        </section>
         );
     };
 
