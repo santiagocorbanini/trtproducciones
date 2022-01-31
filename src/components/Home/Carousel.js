645572eb343c347f9d23a240f36923f3264f82ba
Franco Vanney <francoavanney@gmail.com>
2022-01-31 13:27:59 -0300

    carousel mod

--- src/components/Home/Carousel.js
+++ src/components/Home/Carousel.js
@@ -4,26 +4,39 @@ import booking from "../../assets/img/booking.jpg"
 const Carousel = () => {
     return (  
             <div>
-                <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                    <img src={logo} className="d-block w-100" alt="..."/>
+                <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
+                                <img className="img-carousel" src={booking} alt="..." />
+                            </div>                    
+                        </div>
+                        <div className="carousel-item">
+                            <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
+                                <img className="img-carousel" src={booking} alt="..." />
+                            </div>                    
+                        </div>
+                        <div className="carousel-item">
+                            <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
+                                <img className="img-carousel" src={booking} alt="..." />
+                            </div>
+                        </div>
                     </div>
-                    <div className="carousel-item">
-                    <img src={logo} className="d-block w-100" alt="..."/>
-                    </div>
-                    <div className="carousel-item">
-                    <img src={logo} className="d-block w-100" alt="..."/>
-                    </div>
-                </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Anterior</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Siguiente</span>
-                </button>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Anterior</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Siguiente</span>
+                    </button>
                 </div>
             </div>
     )
