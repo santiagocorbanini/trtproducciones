0e2047e929a940ade4ee9d90b7103acc76d2721f
Franco Vanney <francoavanney@gmail.com>
2022-02-01 11:33:17 -0300

    nav degrade

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,7 +4,7 @@ import logo from '../../assets/img/logo.png';
 const Nav = () => {
     return (
 
-        <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+        <nav className="navbar navbar-expand-lg text-uppercase fixed-top" id="mainNav">
             <div className="container">
                 <a className="navbar-brand" href="#home"><img src={logo} alt={logo} id="logo" /></a>
                 <button className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" id="menu-text" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
