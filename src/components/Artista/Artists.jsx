1003c6f444b246c7850777c4654de403ded6e0ff
Santiago Corbanini <santiagocorbanini@gmail.com>
2022-09-04 17:35:09 +0200

    nueva rama

--- src/components/Artista/Artists.jsx
+++ src/components/Artista/Artists.jsx
@@ -40,6 +40,7 @@ const Artists = () => {
                             </div>
                             <img className="img-fluid" src={artist} alt="..." />
                         </div>
+                        <iframe  src="https://open.spotify.com/embed/track/7qcjZzK6bTXdShN6bdeNgq?utm_source=generator&theme=0" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
                     </div>
                 </div>
             </div>
