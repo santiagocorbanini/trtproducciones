16416b80c84af479ccbaaf8aa124991bf5bf37d9
Franco Vanney <francoavanney@gmail.com>
2022-09-09 11:38:25 -0300

    add carousel para marcas

--- src/Trabajos/index.jsx
+++ src/Trabajos/index.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import Slider from "react-slick";
+import "./style.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Trabajos = () => {
     return (
         <section id="trabajos">
         <div className="container d-flex align-items-center justify-content-center p-5">
             <h2>Trabajos</h2>
+        </div>
+        <div className="container-fluid brands">
+        <Slider
+            autoplay={true}
+            slidesToShow={3}
+            autoplaySpeed={2000}
+            dots={false}
+        >
+            <div>
+            <img src="https://via.placeholder.com/200" alt="" />
+            </div>
+            <div>
+            <img src="https://via.placeholder.com/200" alt="" />
+            </div>
+            <div>
+            <img src="https://via.placeholder.com/200" alt="" />
+            </div>
+            <div>
+            <img src="https://via.placeholder.com/200" alt="" />
+            </div>
+        </Slider>
         </div>
         </section>
     );
